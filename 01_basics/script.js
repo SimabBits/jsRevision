6c8e4e4ce7ead9e866793bfe4fa85365f3540f3d
Simab <129927564+SimabBits@users.noreply.github.com>
2025-06-06 08:03:35 +0000

    first day completed

---
+++ 01_basics/script.js
@@ -0,0 +1,12 @@
+const name = "simab"
+let dob = 13
+var lastName = "Shabbir"
+ph = 9191
+console.log(name, dob, lastName,ph)
+
+//we can use var before declaration and it will store undefined value  by default
+
+//ignore using var insted use let due to scoping issues
+// we can define variables without any keyword but its not recommended
+
+console.table([name,dob,lastName])
