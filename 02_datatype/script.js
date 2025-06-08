aaa2c36e6b181db04dfa827b3d6b4c33eb431e3c
Simab <129927564+SimabBits@users.noreply.github.com>
2025-06-08 15:10:23 +0000

    Studied Data types in js

---
+++ 02_datatype/script.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code as newer version
+
+let str = "This is a string";
+let n = 1399 //this is a number
+let toggle = true; //this is a boolean
+let sit = null; // this is null an object type 
+let dig ; // this is undefined 
+
+let computer = {
+ specs : "hardware",
+ core : 4
+}; //this is an object with specs and core as key and value pairs
+
+
